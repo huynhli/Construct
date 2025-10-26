62968ca0912289d44b8cec3314b4a0aceeff31d5
taeyeonha <taeyeonha10@gmail.com>
2025-10-26 00:50:12 -0400

    projects page: display projects if projects exist in "projects" array

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -12,6 +12,8 @@ interface Project {
 }
 
 export default function HomePage() {
+	const [projects, setProjects] = useState<Project[]>([]);
+	const navigate = useNavigate();
 
     return (
         <div className="min-h-screen bg-zinc-900 text-white">
@@ -22,13 +24,32 @@ export default function HomePage() {
             <main className="p-8">
                 {/* Project Gallery */}
                 <div className="w-full max-w-4xl mx-auto space-y-8">
-					<div className="bg-zinc-800 rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform duration-300">
-						<img
-							src={'https://upload.wikimedia.org/wikipedia/commons/1/14/Brr_brr_patapim.jpg'} // Fallback image
-							alt={"Project Name"}
-							className="w-fit h-64 object-cover"
-						/>
-					</div>
+					{projects.length > 0 ? (
+						projects.map(project => (
+							<div key={project.id} className="bg-zinc-800 rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform duration-300">
+								<img
+									src={'https://upload.wikimedia.org/wikipedia/commons/1/14/Brr_brr_patapim.jpg'} // Fallback image
+									alt={project.name}
+									className="w-fit h-64 object-cover"
+								/>
+								<div className="p-6">
+									<h2 className="text-2xl font-semibold mb-2">{project.name}</h2>
+									<p className="text-zinc-400 mb-4">
+										{project.description || "No description"}
+									</p>
+
+									<div className="text-sm text-sinz-500">
+										</div>
+								</div>
+							</div>
+						)
+
+						)
+					) : (
+						<div className="text-center text-zinc-300">
+							<h2 className="text-2xl">No projects assigned</h2>
+						</div>
+					)}
                 </div>
             </main>
         </div>
